88a7940f5dfae8c6a9028a225616ecc5fdf78774
amiviju <ami.viju@reancloud.com>
2017-09-13 14:17:46 +0530

    Update ubuntu_datadog.rb

--- recipes/ubuntu_datadog.rb
+++ recipes/ubuntu_datadog.rb
@@ -5,6 +5,6 @@ execute 'update' do
   command 'apt-get update -y'
 end
 execute 'installation' do
-  command 'DD_API_KEY=307158ad8f11d65d767982aa4qwerfrrttt bash -c "$(curl -L https://raw.githubusercontent.com/DataDog/dd-agent/master/packaging/datadog-agent/source/install_agent.sh)" >/dev/null
+  command 'DD_API_KEY=307158ad8f11d65d767982aa4qwerfrrttt bash -c "$(curl -s -L https://raw.githubusercontent.com/DataDog/dd-agent/master/packaging/datadog-agent/source/install_agent.sh)" >/dev/null
 '
 end
