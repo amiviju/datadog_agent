9d77dff068fc05ec6e66160fd1b52ea53ab9a166
amiviju <ami.viju@reancloud.com>
2017-09-13 15:49:55 +0530

    add

--- recipes/linux_datadog.rb
+++ recipes/linux_datadog.rb
@@ -1,9 +1,9 @@
-# Cookbook:: datadog_agent
+# Cookbook:: Datadog_agents
 # Recipe:: default
 # Copyright:: 2017, The Authors, All Rights Reserved.
 execute 'update' do
-  command 'yum update -y'
+  command 'apt-get update -y'
 end
 execute 'installation' do
-  command 'DD_API_KEY=307158ad8f11d65d767982aa4qwerfrrttt bash -c "$(curl -s -L https://raw.githubusercontent.com/DataDog/dd-agent/master/packaging/datadog-agent/source/install_agent.sh)"'
+  command 'DD_API_KEY=307158ad8f11d65d767982aa4qwerfrrttt bash -c "$(curl -s -L https://raw.githubusercontent.com/DataDog/dd-agent/master/packaging/datadog-agent/source/install_agent.sh)" >/dev/null; ls'
 end
